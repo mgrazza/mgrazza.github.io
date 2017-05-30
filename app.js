6cd4e1a9a6e16a447ba8db3a9cbde4ac1b4d3da8
mgrazza <Ryan_Gough@hotmail.co.uk>
2017-05-30 18:34:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 var config = {
   // - Your app's id on moneypot.com
-  app_id: 18,                             // <----------------------------- EDIT ME!
+  app_id: 2872,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
   app_name: 'Bitcoin Dice Game MG',
   // - For your faucet to work, you must register your site at Recaptcha
